5c0c93892a2e90bebec1cc7d68d78345db9cd59b
H1zz <s15116129@stu.edu.tw>
2022-01-25 13:01:52 +0800

    0125

---
+++ javascript/header.js
@@ -0,0 +1,22 @@
+MenuShow();
+function MenuShow(){
+    //  $ jQuery的變數前綴詞
+
+    // 隱藏選單與關閉按鈕
+    $('#Navbar_Menu').hide();
+    $('#Mobile_Close').hide();
+
+    // 開啟選單
+    $('#Mobile_Menu').click( function(){
+        $('#Navbar_Menu').show();
+        $('#Mobile_Menu').hide();
+        $('#Mobile_Close').show();        
+    });
+    
+    // 關閉選單
+    $('#Mobile_Close').click( function(){
+        $('#Navbar_Menu').hide();
+        $('#Mobile_Close').hide();
+        $('#Mobile_Menu').show(); 
+    });
+}
