274ab8cdeaaa12fffa9ea7a91f85f03f2bb82f22
H1zz <s15116129@stu.edu.tw>
2022-03-06 08:02:46 +0800

    0306

--- javascript/header.js
+++ javascript/header.js
@@ -8,14 +8,14 @@ function MenuShow(){
 
     // 開啟選單
     $('#Mobile_Menu').click( function(){
-        $('#Navbar_Menu').show();
+        $('#Navbar_Menu').slideToggle(500);
         $('#Mobile_Menu').hide();
         $('#Mobile_Close').show();        
     });
     
     // 關閉選單
     $('#Mobile_Close').click( function(){
-        $('#Navbar_Menu').hide();
+        $('#Navbar_Menu').slideToggle(500);
         $('#Mobile_Close').hide();
         $('#Mobile_Menu').show(); 
     });
